61baf856b97c313ad64a9071a4217c09aaf79813
Tim Strahm <tim-strahm@hotmail.com>
2025-05-07 09:14:22 +0000

    Set up Safety system

---
+++ inc/MyRobotSafetyProperties.hpp
@@ -0,0 +1,50 @@
+#ifndef MyRobotSAFETYPROPERTIES_HPP_
+#define MyRobotSAFETYPROPERTIES_HPP_
+
+#include <eeros/safety/SafetyProperties.hpp>
+#include <eeros/hal/HAL.hpp>
+#include "ControlSystem.hpp"
+
+class MyRobotSafetyProperties : public eeros::safety::SafetyProperties
+{
+public:
+    MyRobotSafetyProperties(ControlSystem &cs, double dt);
+
+    // Define all possible events
+    eeros::safety::SafetyEvent doSystemOff;
+    eeros::safety::SafetyEvent doSystemOn;
+    eeros::safety::SafetyEvent doSystemStopped;
+    eeros::safety::SafetyEvent doSystemStarted;
+    eeros::safety::SafetyEvent doPowerOn;
+    eeros::safety::SafetyEvent doPowerOff;
+    eeros::safety::SafetyEvent doStartMoving;
+    eeros::safety::SafetyEvent doStopMoving;
+    eeros::safety::SafetyEvent doEmergency;
+    eeros::safety::SafetyEvent doResetEmergency;
+
+    
+
+    // Defina all possible levels
+    eeros::safety::SafetyLevel slSystemOff;
+    eeros::safety::SafetyLevel slSystemTurningOn;
+    eeros::safety::SafetyLevel slSystemTurningOff;
+    eeros::safety::SafetyLevel slSystemOn;
+    eeros::safety::SafetyLevel slSystemMotorPowered;
+    eeros::safety::SafetyLevel slSystemMoving;
+    eeros::safety::SafetyLevel slSystemEmergency;
+
+private:
+    // Define all critical outputs
+    eeros::hal::Output<bool>* LED_g;
+    eeros::hal::Output<bool>* LED_r;
+
+
+    // Define all critical inputs
+    eeros::hal::Input<bool>* button_p; // pause button
+    eeros::hal::Input<bool>* button_m; // Mode button
+
+
+    ControlSystem &cs;
+};
+
+#endif // MyRobotSAFETYPROPERTIES_HPP_
---
+++ src/MyRobotSafetyProperties.cpp
@@ -0,0 +1,109 @@
+#include "MyRobotSafetyProperties.hpp"
+
+MyRobotSafetyProperties::MyRobotSafetyProperties(ControlSystem &cs, double dt)
+    : cs(cs),
+    
+      slSystemOff("System is offline"),
+      slSystemTurningOn("System is turning On"),
+      slSystemTurningOff("System is turning Off"),
+      slSystemOn("System is online"),
+      slSystemMotorPowered("Motor is powered"),
+      slSystemMoving("System is moving"),
+      slSystemEmergency("System is in EMERGENCY STATE"),
+
+      doSystemOn("Startup the system"),
+      doSystemOff("Shutting down the system"),
+      doSystemStopped("Stopping and turning off the system"),
+      doSystemStarted("Starting the system"),
+      doEmergency("Emergency occured"),
+      doResetEmergency("Emergency has been reset"),
+      doPowerOn("Power up the motors"),
+      doPowerOff("Power down the motors"),
+      doStartMoving("Start movement"),
+      doStopMoving("Halt movement")
+      
+{
+    eeros::hal::HAL &hal = eeros::hal::HAL::instance();
+
+    // Declare and add critical outputs
+    LED_g = hal.getLogicOutput("onBoardLEDgreen"); // relates to config file names
+    LED_r = hal.getLogicOutput("onBoardLEDred");
+
+    criticalOutputs = {LED_g};//, LED_r};
+
+    // Declare and add critical inputs
+    button_m = eeros::hal::HAL::instance().getLogicInput("onBoardButtonMode"); //trigger emergency
+    button_p = eeros::hal::HAL::instance().getLogicInput("onBoardButtonPause"); //reset emergency
+
+    criticalInputs = {button_m, button_p};
+
+    // Add all safety levels to the safety system
+    // (Order defines Emergency level structure)
+    addLevel(slSystemOff);
+    addLevel(slSystemTurningOn);
+    addLevel(slSystemTurningOff);
+    addLevel(slSystemOn);
+    addLevel(slSystemMotorPowered);
+    addLevel(slSystemMoving);
+    addLevel(slSystemEmergency);
+
+    // Add events to individual safety levels
+    slSystemOff.addEvent(doSystemOn, slSystemTurningOn, kPublicEvent);
+    slSystemTurningOn.addEvent(doSystemStarted, slSystemOn, kPublicEvent);
+    slSystemTurningOff.addEvent(doSystemOff, slSystemOff, kPublicEvent);
+    slSystemOn.addEvent(doPowerOn, slSystemMotorPowered, kPublicEvent);
+    slSystemOn.addEvent(doSystemStopped, slSystemTurningOff, kPublicEvent);
+    slSystemMotorPowered.addEvent(doStartMoving, slSystemMoving, kPublicEvent);
+    slSystemMotorPowered.addEvent(doPowerOff, slSystemOn, kPublicEvent);
+    slSystemMoving.addEvent(doStopMoving, slSystemMotorPowered, kPublicEvent);
+    slSystemEmergency.addEvent(doResetEmergency, slSystemOn, kPublicEvent);
+
+
+    // Add events to multiple safety levels
+    addEventToAllLevelsBetween(slSystemOn, slSystemMoving, doEmergency, slSystemEmergency, kPublicEvent);
+
+    // Define input actions for all levels
+    slSystemOff.setInputActions({ignore(button_p), ignore(button_m)});
+    slSystemTurningOn.setInputActions({ignore(button_p), ignore(button_m)});
+    slSystemTurningOff.setInputActions({ignore(button_p), ignore(button_m)});
+    slSystemOn.setInputActions({check(button_p, false, doEmergency), ignore(button_m)});
+    slSystemMotorPowered.setInputActions({check(button_p, false, doEmergency), ignore(button_m)});
+    slSystemMoving.setInputActions({check(button_p, false, doEmergency), ignore(button_m)});
+    slSystemEmergency.setInputActions({ignore(button_p), check(button_m, false, doResetEmergency)});
+
+    // Define output actions for all levels
+    slSystemOff.setOutputActions({set(LED_g, false), set(LED_r, false)});
+    slSystemTurningOn.setOutputActions({set(LED_g, false), set(LED_r, true)});
+    slSystemTurningOff.setOutputActions({set(LED_g, false), set(LED_r, true)});
+    slSystemOn.setOutputActions({set(LED_g, true), set(LED_r, false)});
+    slSystemMotorPowered.setOutputActions({set(LED_g, true), set(LED_r, true)});
+    slSystemMoving.setOutputActions({set(LED_g, true), set(LED_r, true)});
+    slSystemEmergency.setOutputActions({set(LED_g, true), set(LED_r, true)});
+    
+
+    // Define and add level actions
+    slSystemOff.setLevelAction([&](SafetyContext *privateContext) {
+        cs.timedomain.stop();
+        eeros::Executor::stop();
+    });
+
+    slSystemTurningOn.setLevelAction([&,dt](SafetyContext *privateContext) { // Trans.State during startup
+        if(slSystemTurningOn.getNofActivations()*dt >= 1) //dt increases in 0.001s steps. if >=1, then loop has been done 1000 times
+            {
+              privateContext->triggerEvent(doSystemStarted);  
+            }
+    });
+
+
+    slSystemOn.setLevelAction([&](SafetyContext *privateContext) {
+        cs.timedomain.start();
+    });
+
+    // Define entry level
+    setEntryLevel(slSystemOff);
+
+    // Define exit function
+    exitFunction = ([&](SafetyContext *privateContext) {
+        privateContext->triggerEvent(doSystemOff);
+    });
+}
